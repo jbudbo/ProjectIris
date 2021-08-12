2f5d54b60c5c121a5f06fccfa8c3d524e2d2bbb1
Justin Morris <25177311+jbudbo@users.noreply.github.com>
2021-08-12 09:11:18 -0400

    Ingress generic setup

---
+++ Src/Ingress/IngressWorker.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Ingress
+{
+    internal class IngressWorker : IHostedService
+    {
+        private readonly ILogger<IngressWorker> logger;
+
+        public IngressWorker(ILogger<IngressWorker> logger)
+        {
+            this.logger = logger;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- Src/Ingress/Program.cs
+++ Src/Ingress/Program.cs
@@ -1,12 +1,27 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
 
 namespace Ingress
 {
-    class Program
+    sealed class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        /// <summary>
+        /// Primary Console Start method
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static Task Main(string[] args) => CreateHostBuilder(args).Build()
+            .RunAsync();
+
+        /// <summary>
+        /// Our Generic Host builder
+        /// </summary>
+        /// <param name="args"></param>
+        /// <returns></returns>
+        static IHostBuilder CreateHostBuilder(string[] args) => Host.CreateDefaultBuilder(args)
+            .ConfigureAppConfiguration(cfg => cfg.AddEnvironmentVariables("IRIS_"))
+            .ConfigureServices((_, services) => services.AddHostedService<IngressWorker>());
     }
 }
