0959e68dd03e39e166b26a168563e397f0d4ca79
Justin Morris <25177311+jbudbo@users.noreply.github.com>
2021-08-24 21:49:23 -0400

    more metrics added to dashboard

--- Src/Dashboard/Extensions.cs
+++ Src/Dashboard/Extensions.cs
@@ -20,11 +20,7 @@ internal static class Extensions
                 await next();
                 return;
             }
-
-            var redis = ctxt.RequestServices.GetRequiredService<IConnectionMultiplexer>();
-
-            IDatabase db = redis.GetDatabase();
-
+            
             string reqPath = ctxt.Request?.Path.Value ?? string.Empty;
 
             if (!reqPath.Equals("/datafeed", StringComparison.InvariantCultureIgnoreCase))
@@ -32,6 +28,16 @@ internal static class Extensions
                 await next();
                 return;
             }
+
+            var redis = ctxt.RequestServices.GetRequiredService<IConnectionMultiplexer>();
+
+            IDatabase db = redis.GetDatabase();
+
+            var tweetStart = await db.StringGetAsync("tweetStart");
+
+            var ticksSinceStart = tweetStart.TryParse(out long ticks) ? ticks : 0;
+
+            DateTime timeSinceTweetStart = new(ticksSinceStart, DateTimeKind.Utc);
 
             try
             {
@@ -42,13 +48,30 @@ internal static class Extensions
 
                 while(!ctxt.RequestAborted.IsCancellationRequested)
                 {
+                    TimeSpan secondsSinceStart = DateTime.UtcNow - timeSinceTweetStart;
+
                     ITransaction transaction = db.CreateTransaction();
 
                     var tTweetCount = transaction.StringGetAsync("tweetCount");
+                    var tUrlCount = transaction.StringGetAsync("urlCount");
+                    var tEmojiCount = transaction.StringGetAsync("emojiCount");
+                    var tdomainLeaders = transaction.SortAsync("domains", take: 5, order: Order.Descending);
 
                     await transaction.ExecuteAsync();
 
-                    var anon = new { tweetsReceived = (await tTweetCount).ToString() };
+                    var domainLeaders = await tdomainLeaders;
+
+                    double tweetCount = double.Parse(await tTweetCount);
+                    double urlCount = double.Parse(await tUrlCount);
+                    double emojiCount = double.Parse(await tEmojiCount);
+
+                    var anon = new {
+                        tweetsPerSec = tweetCount / secondsSinceStart.TotalSeconds,
+                        tweetsReceived = tweetCount,
+                        emojiPerc = (emojiCount / tweetCount) * 100.0,
+                        urlPerc = (urlCount / tweetCount) * 100.0,
+                        topDomains = string.Join(',', domainLeaders.Select(v => v.ToString()))
+                    };
 
                     await response!.WriteAsync("data: ", ctxt.RequestAborted);
 
--- Src/Dashboard/wwwroot/js/index.js
+++ Src/Dashboard/wwwroot/js/index.js
@@ -1,10 +1,16 @@
 ﻿((d) => {
     const eTweetsReceived = d.getElementById('tweetsReceived')
+        , eTPS = d.getElementById('tps')
+        , eEmojiPerc = d.getElementById('emojiPerc')
+        , eUrlPerc = d.getElementById('urlPerc')
         , worker = new Worker('js/worker.js');
 
     worker.onmessage = function (e) {
         if (!e || !e.data) return;
 
+        eTPS.innerText = e.data.tweetsPerSec;
         eTweetsReceived.innerText = e.data.tweetsReceived;
+        eEmojiPerc.innerText = e.data.emojiPerc;
+        eUrlPerc.innerText = e.data.urlPerc;
     }
 })(document);
