99abb6f4b9a39615c13081122e886db0a6eb3e95
Justin Morris <25177311+jbudbo@users.noreply.github.com>
2021-08-27 19:52:01 -0400

    Dashboard UTs

--- Src/Dashboard/Middleware/ServerSentEventsMiddleware.cs
+++ Src/Dashboard/Middleware/ServerSentEventsMiddleware.cs
@@ -1,6 +1,9 @@
 ﻿using StackExchange.Redis;
+using System.Runtime.CompilerServices;
 using System.Text.Json;
 
+[assembly: InternalsVisibleTo("Dashboard.Tests")]
+[assembly: InternalsVisibleTo("DynamicProxyGenAssembly2, PublicKey=0024000004800000940000000602000000240000525341310004000001000100c547cac37abd99c8db225ef2f6c8a3602f3b3606cc9891605d02baa56104f4cfc0734aa39b93bf7852f7d9266654753cc297e7d2edfe0bac1cdcf9f717241550e0a7b191195b7667bb4f64bcb8e2121380fd1d9d46ad2d92d2d15605093924cceaf74c4861eff62abf69b9291ed0a340e113be11e6a7d3113e92484cf7045cc7")]
 namespace Dashboard.Middleware;
 
 internal sealed class ServerSentEventsMiddleware
@@ -30,7 +33,8 @@ internal sealed class ServerSentEventsMiddleware
         CancellationToken token = context.RequestAborted;
         IDatabase db = redis.GetDatabase();
 
-        var tweetStart = await db.StringGetAsync("tweetStart");
+        var tweetStart = await db.StringGetAsync("tweetStart")
+            .ConfigureAwait(false);
 
         var ticksSinceStart = tweetStart.TryParse(out long ticks) ? ticks : 0;
 
@@ -40,8 +44,8 @@ internal sealed class ServerSentEventsMiddleware
         {
             var response = context.Response;
 
-            response.Headers.Add("Cache-Control", "no-cache");
-            response.Headers.Add("Content-Type", "text/event-stream");
+            response.Headers.CacheControl = "no-cache";
+            response.Headers.ContentType = "text/event-stream";
 
             ulong msgId = 0;
             while (!token.IsCancellationRequested)
@@ -74,9 +78,9 @@ internal sealed class ServerSentEventsMiddleware
                     ["mentions"] = mentionsTask.GetAwaiter().GetResult(),
                 };
 
-                msgId = await WriteAndIncrementIdAsync(response, msgId, token);
-                await WriteTweetEventAsync(response, token);
-                await WriteTweetDataAsync(response, result, token);
+                msgId = await WriteAndIncrementIdAsync(response, msgId, token).ConfigureAwait(false);
+                await WriteTweetEventAsync(response, token).ConfigureAwait(false);
+                await WriteTweetDataAsync(response, result, token).ConfigureAwait(false);
             }
         }
         catch (TaskCanceledException) { }
---
+++ Testing/Dashboard.Tests/ServerSentEventsMiddlewareTests.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Moq;
+using StackExchange.Redis;
+using Xunit;
+
+namespace Dashboard.Tests
+{
+    using Middleware;
+    using System.IO.Pipelines;
+
+    public class ServerSentEventsMiddlewareTests
+    {
+        [Fact]
+        public async Task Middleware_Ignores_Requests_Without_Accept_Header()
+        {
+            var mRedis = new Mock<IConnectionMultiplexer>();
+
+            var mContext = new Mock<HttpContext>();
+
+            bool pass = false;
+            Task next(HttpContext c)
+            {
+                pass = true;
+                return Task.CompletedTask;
+            }
+
+            var target = new ServerSentEventsMiddleware(next, mRedis.Object);
+
+            await target.InvokeAsync(mContext.Object);
+
+            Assert.True(pass);
+            mRedis.Verify(r => r.GetDatabase(It.IsAny<int>(), It.IsAny<object>()), Times.Never());
+        }
+
+        [Fact]
+        public async Task Middleware_Executes_Server_Sent_Event_Requests()
+        {
+            var mTransaction = new Mock<ITransaction>();
+
+            var mDatabase = new Mock<IDatabase>();
+            mDatabase.Setup(d => d.CreateTransaction(It.IsAny<object>())).Returns(mTransaction.Object);
+            mDatabase.Setup(d => d.StringGetAsync("tweetStart", It.IsAny<CommandFlags>())).ReturnsAsync(10);
+            mDatabase.Setup(d => d.StringGetAsync("tweetCount", It.IsAny<CommandFlags>())).ReturnsAsync(20);
+
+            var mRedis = new Mock<IConnectionMultiplexer>();
+            mRedis.Setup(r => r.GetDatabase(It.IsAny<int>(), It.IsAny<object>())).Returns(mDatabase.Object);
+
+            var mHeaders = new Mock<IHeaderDictionary>();
+            mHeaders.SetupGet(h => h.Accept).Returns("text/event-stream");
+
+            await using MemoryStream ms = new ();
+            PipeWriter pw = PipeWriter.Create(ms);
+            var mResponse = new Mock<HttpResponse>();
+            mResponse.SetupGet(r => r.Headers).Returns(mHeaders.Object);
+            mResponse.SetupGet(r => r.Body).Returns(ms);
+            mResponse.SetupGet(r => r.BodyWriter).Returns(pw);
+
+            var mRequest = new Mock<HttpRequest>();
+            mRequest.SetupGet(r => r.Headers).Returns(mHeaders.Object);
+
+            CancellationTokenSource source = new(10);
+
+            var mContext = new Mock<HttpContext>();
+            mContext.SetupGet(c => c.RequestAborted).Returns(source.Token);
+            mContext.SetupGet(c => c.Request).Returns(mRequest.Object);
+            mContext.SetupGet(c => c.Response).Returns(mResponse.Object);
+
+            bool pass = false;
+            Task next(HttpContext c)
+            {
+                pass = true;
+                return Task.CompletedTask;
+            }
+
+            var target = new ServerSentEventsMiddleware(next, mRedis.Object);
+
+            await target.InvokeAsync(mContext.Object);
+
+            Assert.False(pass);
+            mRedis.Verify(r => r.GetDatabase(It.IsAny<int>(), It.IsAny<object>()), Times.Once());
+            Assert.NotEqual(0, ms.Length);
+        }
+    }
+}
